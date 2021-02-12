fda1cda7e3128662771a802914cdf7fe2fd6e8b7
ATsvetkov <tsvetvl@mail.ru>
2021-02-12 15:38:35 +1000

    ДЗ Урок 1: route hello 1,2,3,4

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//1
+Route::get('/hello1', function () {
+    return "Hello world";
+});
+
+
+//2
+Route::get('/hello2/{name}', function (string $name) {
+    return "Hello, ".$name;
+});
+
+
+//3 как в методичке
+$text='Привет мир';
+$title='Страница 1';
+$s="<!DOCTYPE html>
+<head>
+    <title>$title</title>
+</head>
+<body>
+    <h1>$text</h1>
+	  Lorem ipsum dolor sit amet
+</body>	  
+</html>";	
+
+
+Route::get('/hello3', function () use($text, $title) {
+    return "<!DOCTYPE html>
+<head>
+    <title>$title</title>
+</head>
+<body>
+    <h1>$text</h1>
+	  Lorem ipsum dolor sit amet
+</body>	  
+</html>"; });
+
+
+
+Route::get('/hello4', function () use($text, $title, $s) {
+    return $s; });
